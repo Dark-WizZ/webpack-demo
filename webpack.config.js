627315a92979058a620f590dce3168ca84c2d9e7
Dark-WizZ <mailwithakram@gmail.com>
2022-08-21 07:29:44 +0530

    Working condition

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const htmlWebpackplugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins:[
+    new htmlWebpackplugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
